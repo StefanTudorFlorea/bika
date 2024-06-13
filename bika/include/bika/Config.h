c501ba401a9cfa8d35b91e1895e7d834ca790b12
Stefan-Tudor Florea <s.florea@rooom.com>
2024-06-13 18:28:52 +0200

    Add config

--- bika/include/bika/Config.h
+++ bika/include/bika/Config.h
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <optional>
 #include <sstream>
+#include <string_view>
 #include <string>
 #include <type_traits>
 #include <yaml-cpp/yaml.h>
@@ -14,16 +15,20 @@ namespace bika {
 
 class Config {
 public:
-    Config(const std::string& filename) {
-        _config = YAML::LoadFile(filename);
+    Config(std::string_view filename) 
+        : _filename{filename} {
+        // empty
     }
 
     template<typename T>
     T get(const std::string& path, std::optional<std::string> env = {}, std::optional<T> init = {}) {
 
+        // detect local file changes
+        _config = YAML::LoadFile(_filename);
+
+        // set init as fallback option
         T value{};
         bool found = false;
-
         if (init) {
             value = init.value();
             found = true;
@@ -41,15 +46,18 @@ public:
 
         // search env var
         if(env) {
-            if (char* env_var = std::getenv(env.value().c_str())) {
-                if constexpr (std::is_integral_v<T>)          value = std::stoi(env_var);
-                if constexpr (std::is_floating_point_v<T>)    value = std::stof(env_var);
-                if constexpr (std::is_same_v<T, std::string>) value = std::string(env_var);
+            if (char* envVar = std::getenv(env.value().c_str())) {
+
+                // must convert manually to correct type
+                if constexpr (std::is_integral_v<T>)          value = std::stoi(envVar);
+                if constexpr (std::is_floating_point_v<T>)    value = std::stof(envVar);
+                if constexpr (std::is_same_v<T, std::string>) value = std::string(envVar);
 
                 found = true;
             }
         }
 
+        // no default, no env, no static found
         if (!found) {
             throw std::runtime_error("value not found");
         }
@@ -59,6 +67,7 @@ public:
     }
 
 private:
+    // tokenize text based on delim
     std::vector<std::string> split(const std::string& text, char delim) {
         std::vector<std::string> tokens;
         std::istringstream iss{text};
@@ -72,6 +81,7 @@ private:
 
 private:
     YAML::Node _config;
+    const std::string _filename;
 };
 
 } // ns bika
--- tests/test_bika.cpp
+++ tests/test_bika.cpp
@@ -1,12 +1,44 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 #include "bika/Config.h"
+#include <cstdlib>
 
 
 TEST_CASE("Config") {
     bika::Config config{"config.yml"};
 
-    // value must be there
-    int port = config.get<int>("configuration.ports.web");
-    CHECK(port == 8080);
+    // static value must be there - int
+    CHECK(config.get<int>("configuration.ports.web") == 8080);
+
+    // static not there, take default
+    CHECK(config.get<int>("path.does.not.exist", {}, 42) == 42);
+
+    // static there, take it even if default is present
+    CHECK(config.get<int>("configuration.ports.web", {}, 42) == 8080);
+
+    // nothing found, and no defaults are set, throw exception
+    CHECK_THROWS_AS(config.get<int>("path.does.not.exist"), std::exception);
+
+    // env var overwrites static settings
+    setenv("CUSTOM_PORT", "1234", 1);
+    CHECK(config.get<int>("configuration.ports.web", "CUSTOM_PORT") == 1234);
+
+    // env var overwrites static settings even if we set default
+    setenv("CUSTOM_PORT", "1234", 1);
+    CHECK(config.get<int>("configuration.ports.web", "CUSTOM_PORT", 42) == 1234);
+
+    // env var takes precedence if static not found
+    setenv("CUSTOM_PORT", "1234", 1);
+    CHECK(config.get<int>("path.does.not.exist", "CUSTOM_PORT") == 1234);
+
+    // env var takes precedence if static not found and even if we have default
+    setenv("CUSTOM_PORT", "1234", 1);
+    CHECK(config.get<int>("path.does.not.exist", "CUSTOM_PORT", 42) == 1234);
+
+    // check types
+    CHECK(config.get<std::string>("types.stringVar") == "hello world");
+    CHECK(config.get<int>("types.intVar") == 101);
+    CHECK(config.get<bool>("types.boolVar") == true);
+    CHECK(config.get<std::vector<int>>("types.listIntVar") == std::vector<int>{1,2,3,4});
+    CHECK(config.get<std::vector<std::string>>("types.listStringVar") == std::vector<std::string>{"a", "b", "c"});
 }
